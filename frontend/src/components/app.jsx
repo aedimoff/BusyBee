335327e773a47ef3dbe7b4ec4227d62d5b74546d
Arianne Dimoff <ariannedimoff@gmail.com>
2021-06-16 11:52:28 -0700

    fix map

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -8,12 +8,21 @@ import LoginFormContainer from './session/login_container';
 import SignupFormContainer from './session/signup_container';
 import Modal from './modal/modal';
 import { Route, Redirect } from 'react-router-dom';
+import Search from './map/search'
+import Map from './map/map'
+require('dotenv').config()
+
+
 
 const App = () => (
     <div className="App">
       <Modal />
       <header>
         <Route path="/" component={NavBarContainer}></Route>
+        <div>
+          {Search()}
+          {Map()}
+        </div>
       </header>
       <Switch>
           <AuthRoute exact path="/" component={Splash} />
