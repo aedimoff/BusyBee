898d9407fa5610d46a49e963e6d48b496d58ab41
jackiemarsh <your_email_herejackie.m.marsh@gmail.com>
2021-06-16 11:05:13 -1000

    Add favorites component

--- frontend/src/components/main/favorites_index.jsx
+++ frontend/src/components/main/favorites_index.jsx
@@ -12,22 +12,29 @@ class FavoritesIndex extends React.Component {
         // some function to add to selected if we're still doing that
         // selected add to "selected" array, which interacts with directions api, 
     }
+
     removeFromSelected() {
         // if selected 
     }
 
     render() {
-        // if (this.props.favorites.length) {
-        //     return (
+        if (this.props.favorites.length) {
+            return (
         //         <div> 
         //              {this.props.favorites.map(fav =>
         //              <i onClick = addToFavorite>heart icon </i> <BusinessItem key={place_id} className=fav-index/>
         //              <button onClick={this.addToSelected} <i check mark icon ></i>>{fav}</button>
         //             )}
         //         </div>
-               
-        //     )
-        // }
+               <>
+               <button className="select-button"><i className="far fa-check-square"></i></button>
+               <button className="favorite-button"><i class="far fa-heart"></i></button>
+               <h1>
+               List of favorite businesses here
+               </h1>
+               </>
+            )
+        }
     }
 }
 
