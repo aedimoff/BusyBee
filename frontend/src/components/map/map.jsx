10a477d1f7e0dd0772717734d40d192b7e0e34b9
Arianne Dimoff <ariannedimoff@gmail.com>
2021-06-30 18:04:21 -0700

    work on centering

--- frontend/src/components/map/map.jsx
+++ frontend/src/components/map/map.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./map.scss";
 
-
 import Search from "./search";
 import Locate from "./locate";
 import calcRoute from "./render";
@@ -9,10 +8,7 @@ import mapStyles from "./mapStyles";
 import * as MapAPIUtil from "../../util/map_api_util";
 import Spinner from "../spinner/spinner";
 
-import {
-  GoogleMap,
-  useLoadScript,
-} from "@react-google-maps/api";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
 
 require("dotenv").config();
 
@@ -21,8 +17,6 @@ const mapContainerStyle = {
   width: "100vw",
   height: "84vh",
 };
-
-
 
 // let center = {
 //   lat: 37.774929,
@@ -40,25 +34,32 @@ const mapContainerStyle = {
 //     lng: -122.419418
 //   }
 // })
-  var center;
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          center = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-        },
+var center;
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      if (position) {
+        center = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+      } else {
+        center = {
+          lat: 37.774929,
+          lng: -122.419418,
+        };
 
-      );
-    } else {
-      center = {
+      }
+    },
+   
+    
+  );
+} else {
+  center = {
     lat: 37.774929,
-    lng: -122.419418
-  }
-    }
- 
-
+    lng: -122.419418,
+  };
+}
 
 const options = {
   styles: mapStyles,
@@ -67,61 +68,55 @@ const options = {
 };
 
 const MapThing = (props) => {
-  console.log(center)
+  console.log(center);
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY,
     libraries,
   });
 
   const mapRef = React.useRef();
-  const onMapLoad = ((map) => {
+  const onMapLoad = (map) => {
     mapRef.current = map;
-  });
+  };
 
-  const panTo = (({ lat, lng }) => {
+  const panTo = ({ lat, lng }) => {
     mapRef.current.panTo({ lat, lng });
     mapRef.current.setZoom(20);
-  });
+  };
 
-    const getFavorite = (placeId) => {
-        MapAPIUtil.getPlaceInfo(placeId).then(res => { 
-            return props.addFavorite(res.data.result, props.userId) 
-        }).catch(err =>
-            console.log(err)
-        )
-    }
+  const getFavorite = (placeId) => {
+    MapAPIUtil.getPlaceInfo(placeId)
+      .then((res) => {
+        return props.addFavorite(res.data.result, props.userId);
+      })
+      .catch((err) => console.log(err));
+  };
 
-    if (loadError) return "Error loading maps";
-    if (!isLoaded) return "Loading Maps";
+  if (loadError) return "Error loading maps";
+  if (!isLoaded) return "Loading Maps";
 
+  const display = isLoaded ? (
+    <div className="map" id="map">
+      <Search panTo={panTo} />
+      <Locate panTo={panTo} />
+      <button onClick={calcRoute}>Test Route</button>
 
-    
-  const display = isLoaded ? 
-        (<div className="map" id="map">
-
-          <Search panTo={panTo} />
-          <Locate panTo={panTo} />
-          <button onClick={calcRoute}>Test Route</button>
-
-          <GoogleMap
-            mapContainerStyle={mapContainerStyle}
-            zoom={14}
-            center={center}
-            options={options}
-            onClick={(e) => {
-                getFavorite(e.placeId)
-            }}
-            onLoad={onMapLoad}
-          />
-      </div>) : <Spinner />
-
-
-  return (
-    <div className="map-container">
-        {display}
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={14}
+        center={center}
+        options={options}
+        onClick={(e) => {
+          getFavorite(e.placeId);
+        }}
+        onLoad={onMapLoad}
+      />
     </div>
+  ) : (
+    <Spinner />
   );
+
+  return <div className="map-container">{display}</div>;
 };
 
 export default MapThing;
-
