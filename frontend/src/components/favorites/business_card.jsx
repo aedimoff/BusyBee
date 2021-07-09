139e20d2dbcae8b28058076ae4c6379e860b36db
Arianne Dimoff <ariannedimoff@gmail.com>
2021-07-09 14:53:49 -0700

    updated selected icons to toggle with state

--- frontend/src/components/favorites/business_card.jsx
+++ frontend/src/components/favorites/business_card.jsx
@@ -1,57 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineHeart } from "react-icons/ai";
 // import { AiFillHeart } from 'react-icons/ai';
 import { AiOutlineCheckSquare, AiFillCheckSquare } from "react-icons/ai";
 import "../main/main.scss";
 
-class BusinessCard extends React.Component {
+const BusinessCard = (props) => {
+  const {
+    formatted_address,
+    favorite,
+    place_id,
+    user_id,
+    name,
+    rating,
+    website,
+    deleteFavorite,
+    receiveSelected,
+    deleteSelected,
+  } = props;
 
-  render() {
-    const {
-      formatted_address,
-      favorite,
-      selected,
-      place_id,
-      user_id,
-      name,
-      rating,
-      website,
-      deleteFavorite,
-      receiveSelected,
-      deleteSelected,
-    } = this.props;
-    return (
-      <div className="business-card-container">
-        <div className="business-card">
-          <div className="click-icons">
-            <p>Remove Favorite</p>
-            <AiOutlineHeart
-              size={18}
-              onClick={() => deleteFavorite(place_id, user_id)}
-              className="heart-icon"
-            />
-            {selected.includes(favorite) ? (
+   const [isSelected, setIsSelected] = useState(false);
+
+   function handleClick() {
+     setIsSelected(!isSelected)
+     if(isSelected) {
+       deleteSelected(favorite)
+     } else {
+       receiveSelected(favorite);
+     }
+   }
+
+  return (
+    <div className="business-card-container">
+      <div className="business-card">
+        <div className="click-icons">
+          <p>Remove Favorite</p>
+          <AiOutlineHeart
+            size={18}
+            onClick={() => deleteFavorite(place_id, user_id)}
+            className="heart-icon"
+          />
+          <div className="check-icon">
+            {isSelected ? (
               <AiFillCheckSquare
                 size={18}
-                onClick={() => deleteSelected(favorite)}
-                className="check-icon"
+                onClick={() => handleClick()}
               />
             ) : (
               <AiOutlineCheckSquare
                 size={18}
-                onClick={() => receiveSelected(favorite)}
-                className="check-icon"
+                onClick={() => handleClick()}
               />
             )}
           </div>
-          <h1 className="business-title">{name}</h1>
-          <h3 className="business-address">{formatted_address}</h3>
-          <h3 className="business-rating">Rating: {rating}</h3>
-          <h3> {website}</h3>
         </div>
+        <h1 className="business-title">{name}</h1>
+        <h3 className="business-address">{formatted_address}</h3>
+        <h3 className="business-rating">Rating: {rating}</h3>
+        <h3> {website}</h3>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
 
 export default BusinessCard;
