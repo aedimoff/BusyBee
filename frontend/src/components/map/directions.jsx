6777a448679128b7873b674677d9b1cab9b2e8f9
patschramm <>
2021-07-09 11:17:07 -0700

    added next prev functionality

--- frontend/src/components/map/directions.jsx
+++ frontend/src/components/map/directions.jsx
@@ -1,63 +1,63 @@
-import React from 'react';
-import useForceUpdate from 'use-force-update';
+import React, { useState } from "react";
 
-const Directions = (props) =>  {
+var i = 0;
 
-  const forceUpdate = useForceUpdate();
-  var i = 0;
-
-  function incrementI(click) {
-      if (click === "next") {
-        i++;
-      } else {
-        i--;
-        }
-      return i;
-  }
-
-  async function handleClick(click) {
-    await incrementI(click); 
-    forceUpdate()
-  }
-
-  let leg = props.directions[i];
-  console.log("iiii", i)
-
-      return (
-        <div className="LEGS">
-          <h1>
-            <p>Destination: {props.directions[i]?.end_address}</p>
-            <p>Distance: {props.directions[i]?.distance.text}</p>
-            <p>Duration: {props.directions[i]?.duration.text}</p>
-          </h1>
-          <button onClick={() => handleClick("prev")}>Previous Leg</button>
-          <button onClick={() => handleClick("next")}>Next Leg</button>
-          <ul className="steps">
-            {props.directions[i]?.steps.map((step, i) => (
-              <h3 key={i} className="step">
-                {step.instructions
-                  .split("<b>")
-                  .join(" ")
-                  .split("</b>")
-                  .join(" ")
-                  .split('<div style="font-size:0.9em">')
-                  .join(" ")
-                  .split("</div>")
-                  .join(" ")
-                  .split("&nbsp;")
-                  .join(" ")}
-                Distance: {step.distance.text}{" "}
-              </h3>
-            ))}
-          </ul>
-        </div>
-      );
-     
-  
-
-
+function useForceUpdate() {
+  const [value, setValue] = useState(0);
+  return () => setValue((i) => i + 1);
 }
 
+const Directions = (props) => {
+  const forceUpdate = useForceUpdate();
 
+  function fuckingWork(n) {
+    i += n;
+    forceUpdate();
+  }
+
+  console.log("in fuckingwork", Object.keys(props.directions).length);
+  let leg = props.directions[i];
+
+  return (
+    <div className="LEGS">
+      {console.log("LORD HAVE MERCY", props.directions.length)}
+      <h1>
+        <p>Destination: {leg?.end_address}</p>
+        <p>Distance: {leg?.distance.text}</p>
+        <p>Duration: {leg?.duration.text}</p>
+      </h1>
+      <div>
+        {i > 0 ? (
+          <button onClick={() => fuckingWork(-1)}>Previous Leg</button>
+        ) : (
+          "fuck"
+        )}
+        {i <= Object.keys(props.directions).length ? (
+          <button onClick={() => fuckingWork(1)}>Next Leg</button>
+        ) : (
+          Object.keys(props.directions).length
+        )}
+      </div>
+      <ul className="steps">
+        {props.directions[i]?.steps.map((step, i) => (
+          <h3 key={i} className="step">
+            {step.instructions
+              .split("<b>")
+              .join(" ")
+              .split("</b>")
+              .join(" ")
+              .split('<div style="font-size:0.9em">')
+              .join(" ")
+              .split("</div>")
+              .join(" ")
+              .split("&nbsp;")
+              .join(" ")}
+            Distance: {step.distance.text}{" "}
+          </h3>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Directions;
